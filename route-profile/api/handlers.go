941a9329355a02eb894bdff17ef14b167a2b493e
Quinn Chrzan <quinnchrzan@gmail.com>
2018-06-07 10:16:28 -0400

    Adding handler to return 3d polyline instead of height array

--- route-profile/api/handlers.go
+++ route-profile/api/handlers.go
@@ -17,13 +17,13 @@ func Index(w http.ResponseWriter, r *http.Request) {
 // GetElevation takes an API request and returns the elevation along the
 // supplied geometry.
 func GetElevation(w http.ResponseWriter, r *http.Request) {
-	GetRaster("elevation", w, r)
+	GetProfile("elevation", w, r)
 }
 
 // GetWind takes a request and returns the headwind in m/s along the
 // supplied geometry.
 func GetWind(w http.ResponseWriter, r *http.Request) {
-	GetRaster("wind", w, r)
+	GetProfile("wind", w, r)
 }
 
 // GetSegments takes a geometry and a resolution and returns an array
@@ -59,10 +59,9 @@ func GetSegments(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// GetRaster is used by GetWind and GetElevations, it handles splitting the
+// GetProfile is used by GetWind and GetElevations, it handles splitting the
 // request geometry and parallelizing the underlying raster calculations.
-func GetRaster(raster string, w http.ResponseWriter, r *http.Request) {
-	var result []float64
+func GetProfile(raster string, w http.ResponseWriter, r *http.Request) {
 	var req Request
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
@@ -72,8 +71,15 @@ func GetRaster(raster string, w http.ResponseWriter, r *http.Request) {
 
 	geom := req.Geometry
 	reqRes := req.Resolution
+	format := req.Format
 	length := db.GeometryLength(geom)
 	resolution := length / 100.0
+
+	if format == "" {
+		format = "array"
+	}
+
+	fmt.Printf("Format: %v\n", format)
 
 	// Prevent oversampling
 	if resolution < 0.00025 {
@@ -89,6 +95,62 @@ func GetRaster(raster string, w http.ResponseWriter, r *http.Request) {
 	goal := samples / (length / resolution)
 	goal *= db.GeographyLength(geom)
 
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	if format == "array" {
+		result := GetRaster(raster, geom, resolution, goal)
+		if err := json.NewEncoder(w).Encode(result); err != nil {
+			panic(err)
+		}
+	} else {
+		result := GetPolyline(geom, resolution)
+		if err := json.NewEncoder(w).Encode(result); err != nil {
+			panic(err)
+		}
+	}
+}
+
+// GetPolyline returns a 3d polyline for a particular geometry
+func GetPolyline(geom string, resolution float64) string {
+	length := db.GeometryLength(geom)
+	samples := 1000.0
+	goal := samples / (length / resolution)
+	goal *= db.GeographyLength(geom)
+
+	rasterFunc := func(geom geometry.SubGeometry, res float64, out chan geometry.SubGeometry) <-chan geometry.SubGeometry {
+		out <- db.ElevationGeometry(geom, resolution)
+		return out
+	}
+
+	segments := db.SplitSegments(geom, goal)
+
+	total := len(segments)
+	out := make(chan geometry.SubGeometry)
+	var geometrySegments geometry.SubGeometries
+
+	for _, segment := range segments {
+		go rasterFunc(segment, resolution, out)
+	}
+
+	count := 0
+	for segment := range out {
+		geometrySegments = append(geometrySegments, segment)
+		count++
+		if count == total {
+			close(out)
+		}
+	}
+
+	sort.Sort(geometrySegments)
+
+	line := db.Combine(geometrySegments)
+	result := db.EncodePolyline(line.Geometry)
+	return result
+}
+
+// GetRaster returns the raster values for a particular raster layer and polyline
+func GetRaster(raster string, geom string, resolution float64, goal float64) []float64 {
 	rasterFunc := func(raster string, geom geometry.SubGeometry, res float64, out chan geometry.RasterSegment) <-chan geometry.RasterSegment {
 		if raster == "wind" {
 			out <- db.Wind(geom, resolution)
@@ -101,6 +163,8 @@ func GetRaster(raster string, w http.ResponseWriter, r *http.Request) {
 	segments := db.SplitSegments(geom, goal)
 	total := len(segments)
 	out := make(chan geometry.RasterSegment)
+
+	var result []float64
 	var rasterSegments geometry.RasterSegments
 
 	for _, segment := range segments {
@@ -122,10 +186,5 @@ func GetRaster(raster string, w http.ResponseWriter, r *http.Request) {
 		result = append(result, segment.Values...)
 	}
 
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-
-	if err := json.NewEncoder(w).Encode(result); err != nil {
-		panic(err)
-	}
+	return result
 }
